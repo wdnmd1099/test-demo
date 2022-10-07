32dee73daf080f06c2c17d822c7d6dcacf8b723c
kasan <879611700@qq.com>
2022-10-07 23:38:56 +0800

    fist

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import { Transition } from 'vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    // vueJsx({
+    //   transformOn:true,
+    //   mergeProps:true,
+    // }),
+  ]
+})
